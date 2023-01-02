daa5543072b614245b659c0e0c1f1c1d0785e994
Dheeraj Sharma <at60sharwan@gmail.com>
2023-01-02 11:03:58 +0530

    Basic JavaScript Practice.

---
+++ JavaScript Absolute Beginner Challenges/Q_23.js
@@ -0,0 +1,5 @@
+//* Write a JavaScript program to get the website URL(loading page).
+
+//*********Solution********
+const htmlText = document.querySelector("h1");
+htmlText.textContent = document.URL;
